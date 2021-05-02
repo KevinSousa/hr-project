00f5d9fdd596f9d1fbcc15b857cc609205e6cdfe
KevinSousa <kevinsmoura@hotmail.com>
2021-05-02 13:23:17 -0300

    Feign implementation

---
+++ hr-payroll/src/main/java/br/com/hr/payroll/feignClients/WorkerFeignClient.java
@@ -0,0 +1,18 @@
+package br.com.hr.payroll.feignClients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import br.com.hr.payroll.entities.Worker;
+
+@Component
+@FeignClient(name = "hr-worker", url = "http://localhost:8001", path = "/workers")
+public interface WorkerFeignClient {
+
+	@GetMapping(value = "/{id}")
+	ResponseEntity<Worker> findById(@PathVariable Long id);
+	
+}
--- hr-payroll/src/main/java/br/com/hr/payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/hr/payroll/services/PaymentService.java
@@ -1,13 +1,26 @@
 package br.com.hr.payroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import br.com.hr.payroll.entities.Payment;
+import br.com.hr.payroll.entities.Worker;
+import br.com.hr.payroll.feignClients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
-
+	
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+	
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Bob", 200.0, days);
+		
+		Worker worker = workerFeignClient.findById(workerId).getBody();
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
