e35f1e6c1af701cae827f098ce1c727d3bd84ab0
KevinSousa <kevinsmoura@hotmail.com>
2021-05-04 11:52:13 -0300

    microservices random port configuration

--- hr-payroll/src/main/java/br/com/hr/payroll/feignClients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/br/com/hr/payroll/feignClients/WorkerFeignClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import br.com.hr.payroll.entities.Worker;
 
 @Component
-@FeignClient(name = "hr-worker", url = "http://localhost:8001", path = "/workers")
+@FeignClient(name = "hr-worker", path = "/workers")
 public interface WorkerFeignClient {
 
 	@GetMapping(value = "/{id}")
