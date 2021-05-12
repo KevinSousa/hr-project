eaf88af5bb9b78d97a0d3256e36894f61b5f1c11
KevinSousa <kevinsmoura@hotmail.com>
2021-05-12 10:16:02 -0300

    hr-config server implementation

--- hr-config-server/src/main/java/br/com/hr/configserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/br/com/hr/configserver/HrConfigServerApplication.java
@@ -2,8 +2,10 @@ package br.com.hr.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class HrConfigServerApplication {
 
 	public static void main(String[] args) {
