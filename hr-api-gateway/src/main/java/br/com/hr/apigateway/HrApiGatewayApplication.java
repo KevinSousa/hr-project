8ad89342f3025f07573a9c2d707d6e162fe6fc8b
KevinSousa <kevinsmoura@hotmail.com>
2021-05-12 09:26:32 -0300

    api gateway creation

---
+++ hr-api-gateway/src/main/java/br/com/hr/apigateway/HrApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.hr.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatewayApplication.class, args);
+	}
+
+}
