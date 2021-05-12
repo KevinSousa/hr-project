d7e9714a5e8e461af3eabcf00226a7b4ab549f21
KevinSousa <kevinsmoura@hotmail.com>
2021-05-12 09:47:44 -0300

    zuul gateway implementation

--- hr-api-gateway/src/main/java/br/com/hr/apigateway/HrApiGatewayApplication.java
+++ hr-api-gateway/src/main/java/br/com/hr/apigateway/HrApiGatewayApplication.java
@@ -2,8 +2,12 @@ package br.com.hr.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
 public class HrApiGatewayApplication {
 
 	public static void main(String[] args) {
